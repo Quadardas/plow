23b65e54b740ae9d1c5e16c2f1e73635013f82d0
quadardas <akladskiy@gmail.com>
2022-12-07 19:29:16 +0500

    123

--- plow/src/components/Header.vue
+++ plow/src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="header">aboba</div>
+  <div class="header">
+    <div class="header__list">
+      <li>1</li>
+      <li>2</li>
+    </div>
+  </div>
 </template>
 <script></script>
 <style></style>
